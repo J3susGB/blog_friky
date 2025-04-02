ee04082ca4990f0caebb326424d6845d9895b684
Jesús Gómez Beltrán <jgomezbeltran88@gmail.com>
2025-04-02 13:43:42 +0200

    Estudio de algunos métodos mágicos de Sinfony. Closes #10

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class PostController extends AbstractController{
+final class PostController extends AbstractController {
     
-    #[Route('/post', name: 'app_post')]
-    public function index(): Response
+    private $em;
+
+    public function __construct(EntitymanagerInterface $em) {
+        $this->em = $em;
+    }
+
+    #[Route('/post/{id?0}', name: 'app_post')]
+    public function index($id): Response
     {
+
+        $posts = $this->em->getRepository(Post::class)->findBy(['id' => 1, 'titulo' => 'Mi primer post de prueba']);
+
+        if (!$posts) {
+            throw $this->createNotFoundException("Post no encontrado");
+        }
+
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts' => $posts,
         ]);
     }
 }
